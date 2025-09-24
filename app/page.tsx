ef112e728c1e9593194d0d8a789b97d5f6546d61
romain mtz <mail@romainmartinez.com>
2025-09-24 08:46:02 +0200

    Double fusee

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <h1>Hello Hello World MatTribe 🚀</h1>
+      <h1>Hello 🚀🚀 World MatTribe 🚀</h1>
     </main>
   );
 }
