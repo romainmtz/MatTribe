188d4c5535f6a92e47c3c65cf6088428b9b08c82
romain mtz <mail@romainmartinez.com>
2025-09-24 11:18:31 +0200

    Logo in header first test

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
     <header className="bg-white shadow p-4 flex justify-between items-center">
-      <h1 className="text-xl font-semibold">MatTribe</h1>
+      <img
+        src="/logo.png"
+        alt="MatTribe Logo"
+        className="h-10 w-auto"
+      />
       <nav className="space-x-4">
         <a href="/" className="text-blue-600 hover:underline">Accueil</a>
         <a href="/about" className="text-blue-600 hover:underline">À propos</a>
