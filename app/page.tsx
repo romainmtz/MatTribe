6d87f8c084ee23dcffa5cd8478c565aa759517fd
romain mtz <mail@romainmartinez.com>
2025-09-24 08:36:09 +0200

    change texte

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <h1>Hello World MatTribe 🚀</h1>
+      <h1>Hello Hello World MatTribe 🚀</h1>
     </main>
   );
 }
